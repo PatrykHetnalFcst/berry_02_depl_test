c9a1f6968c2c991b745e19e20e7a761beadb263e
PatrykHetnalFcst <patryk.hetnal@forecast.global>
2022-06-08 17:15:47 +0200

    commit

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from app import create_app, db
 from app.auth.models import Kierowca
 
-if __name__ == '__main__': ## Tabs changed
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not Kierowca.query.filter_by(user_name='Łukasz').first():
-            Kierowca.create_user(user='Łukasz',
-                                city='Tychy',
-                                password='TychyDzienKobiet')
-    flask_app.run()
+
+flask_app = create_app('dev')
+with flask_app.app_context():
+    db.create_all()
+    if not Kierowca.query.filter_by(user_name='Łukasz').first():
+        Kierowca.create_user(user='Łukasz',
+                            city='Tychy',
+                            password='TychyDzienKobiet')
+flask_app.run()
